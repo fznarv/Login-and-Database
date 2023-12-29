a2d4d0881f2e8bac403b65e740ed9f03495aeba9
Narvin C. Alamas <narvinalamas@gmail.com>
2023-12-29 22:27:17 +0800

    first commit for login.py(OOP and method)

--- Index.py
+++ Index.py
@@ -9,16 +9,18 @@
 #     except ValueError:
 #         print('Invalid Input!')
 
-import tkinter as tk
-from tkinter import messagebox
+# import tkinter as tk
+# from tkinter import messagebox
 
-# Create a tkinter root window
-root = tk.Tk()
-root.withdraw()  # Hide the root window
+# # Create a tkinter root window
+# root = tk.Tk()
+# root.withdraw()  # Hide the root window
 
-# Show a popup message
-messagebox.showinfo("Popup Title", "This is a popup message!")
-# messagebox.showwarning('Warning!', 'This is a Warning!')
-messagebox.showerror('Error!', 'This is an Error!')
-# Run the tkinter main loop
-root.mainloop()
+# # Show a popup message
+# messagebox.showinfo("Popup Title", "This is a popup message!")
+# # messagebox.showwarning('Warning!', 'This is a Warning!')
+# messagebox.showerror('Error!', 'This is an Error!')
+# # Run the tkinter main loop
+# root.mainloop()
+
+
---
+++ login.py
@@ -0,0 +1,33 @@
+class Login:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def login_key(self):
+        with open('Login.txt', 'r') as file:
+            file.seek(0)
+            content = file.readlines()
+            counter = 0
+            number_of_tries = 3
+            try:
+                if not content:
+                    number_of_tries -= 1
+
+                    while counter < 3:
+                        print(f'Login Failed! You have {number_of_tries} tries left.')
+                        counter += 1
+                        
+                        if number_of_tries == 0:
+                            print(f'You have {number_of_tries} tries left. Program exit.')
+                            break
+                        else:
+                            Lwelcome = Login()
+
+                            print(f'Welcome {Lwelcome.username}!')
+                else:
+                           
+            except ValueError:
+                print('Yikes')
+            
+
