b9c1e042dafbbe41e10d5e0c808e9da372c9fa6f
Narvin C. Alamas <narvinalamas@gmail.com>
2023-12-29 21:54:40 +0800

    CRUD

---
+++ Index.py
@@ -0,0 +1,24 @@
+# method for login
+
+# method for database using MySql + OOP
+
+# while True:
+#     try:
+#         print('')
+
+#     except ValueError:
+#         print('Invalid Input!')
+
+import tkinter as tk
+from tkinter import messagebox
+
+# Create a tkinter root window
+root = tk.Tk()
+root.withdraw()  # Hide the root window
+
+# Show a popup message
+messagebox.showinfo("Popup Title", "This is a popup message!")
+# messagebox.showwarning('Warning!', 'This is a Warning!')
+messagebox.showerror('Error!', 'This is an Error!')
+# Run the tkinter main loop
+root.mainloop()
